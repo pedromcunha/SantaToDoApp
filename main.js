5af07059b1903bb8ab66453e68cd8db031714060
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-11-12 23:40:28 -0500

    can now add kids

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 //Main js file
+//Let's break some functions out to reuse
+var isNaughtyOrNice = function () {
+	event.preventDefault(); //Just like we did above we have to stop the anchor tag from navigating away
+	//Store the icon that's been clicked.
+	var clickedIcon = $(this).children('i');
+	//Now we can check to see if it is a naughty or nice decision
+	if(clickedIcon.hasClass('fa-thumbs-o-up')) {
+		//then we can remove the font awesome class that makes it transparent and add the solid font class
+		clickedIcon.removeClass('fa-thumbs-o-up');
+		clickedIcon.addClass('fa-thumbs-up');
+		//Now we can completely remove the other decision, santa won't need that
+		$(this).parent().find('.fa-thumbs-o-down').remove();
+	}
+	//same as above but reverse
+	else {
+		clickedIcon.removeClass('fa-thumbs-o-down');
+		clickedIcon.addClass('fa-thumbs-down');
+		$(this).parent().find('.fa-thumbs-o-up').remove();
+	}
+};
 
 //Set up the tabulation
 $('footer').find('a').bind('click', function(event) {
@@ -19,22 +39,26 @@ $('footer').find('a').bind('click', function(event) {
 });
 
 //Set up the interaction for choosing naughty or nice
-$('#children').find('a').bind('click', function() {
-	event.preventDefault(); //Just like we did above we have to stop the anchor tag from navigating away
-	//Store the icon that's been clicked.
-	var clickedIcon = $(this).children('i');
-	//Now we can check to see if it is a naughty or nice decision
-	if(clickedIcon.hasClass('fa-thumbs-o-up')) {
-		//then we can remove the font awesome class that makes it transparent and add the solid font class
-		clickedIcon.removeClass('fa-thumbs-o-up');
-		clickedIcon.addClass('fa-thumbs-up');
-		//Now we can completely remove the other decision, santa won't need that
-		$(this).parent().find('.fa-thumbs-o-down').remove();
-	}
-	//same as above but reverse
-	else {
-		clickedIcon.removeClass('fa-thumbs-o-down');
-		clickedIcon.addClass('fa-thumbs-down');
-		$(this).parent().find('.fa-thumbs-o-up').remove();
-	}
+$('#children').find('a').bind('click', isNaughtyOrNice);
+
+//Let's make it so Santa can add new children to the naughty or nice list
+$('#addChildrenBtn').bind('click', function(event) {
+	event.preventDefault(); //As always we have to prevent the button from doing it's normal behaviour
+
+	//Now let's get some data, we need the kid's name that was in the form, the list of kids and the template for the list
+	//Notice how we could've used one var keyword, this improves performance (but not by much), readability is more important
+	var kidName = $( "form[name='children-form']" ).find('input').val();
+	var	unorderedList = $('#children').find('.list-group');
+	var	newDomNode = "<li class='list-group-item' id='recently-added'>" + 
+					kidName + 
+					'<a href><i class="fa fa-thumbs-o-down pull-right"></i></a>' +
+					'<a href><i class="fa fa-thumbs-o-up pull-right"></i></a> </li>';
+	//now let's append the concatenated template to the unordered list
+	$(newDomNode).appendTo(unorderedList);
+	//Next we have to reuse the function callback we created and used above so that the new element has the same behaviour
+	$('#recently-added').find('a').bind('click', isNaughtyOrNice);
+	//Finally we can remove the unique ID we added to identify a new kid added
+	$('#recently-added').removeAttr('id');
 });
+
+
