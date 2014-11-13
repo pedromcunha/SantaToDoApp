d206db67ea57f281ce539a4941ad3399600d8e48
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-11-12 22:50:41 -0500

    can now rate naughty or nice for children

--- main.js
+++ main.js
@@ -17,3 +17,25 @@ $('footer').find('a').bind('click', function(event) {
 	//Add active only to the icon that we are click on
 	$(this).children('i').addClass('active');
 });
+
+//Set up the interaction for choosing naughty or nice
+$('#children').find('a').bind('click', function() {
+	event.preventDefault(); //Just like we did above we have to stop the anchor tag from navigating away
+	//Store the icon that's been clicked.
+	var clickedIcon = $(this).children('i');
+	//Now we can check to see if it is a naughty or nice decision
+	if(clickedIcon.hasClass('fa-thumbs-o-up')) {
+		//then we can remove the font awesome class that makes it transparent and add the solid font class
+		clickedIcon.removeClass('fa-thumbs-o-up');
+		clickedIcon.addClass('fa-thumbs-up');
+		//Now we can completely remove the other decision, santa won't need that
+		$(this).parent().find('.fa-thumbs-o-down').remove();
+	}
+	//same as above but reverse
+	else {
+		clickedIcon.removeClass('fa-thumbs-o-down');
+		clickedIcon.addClass('fa-thumbs-down');
+		$(this).parent().find('.fa-thumbs-o-up').remove();
+	}
+	$(this).find('i').addClass('active');
+});
